c9683b00a0dc02fc68c86149f6e2942d0d1f7089
Nitish Kumar <niku@Nitishs-MacBook-Air.local>
2022-10-20 12:53:03 +0530

    done

--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -4,7 +4,7 @@ import { IUser } from "./user.model";
 const userSchema = new mongoose.Schema<IUser>({
   name: { type: String, required: true },
   age: { type: Number, required: true },
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   mobile: { type: Number, required: true },
   lastUpdatePasswordDate: { type: Date, default: null },
